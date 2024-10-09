3481cd2b7f2d11962e484376b82a58cd97b0caeb
rafia9005 <rafia9005@gmail.com>
2024-10-09 22:14:10 +0700

    fix

--- main.js
+++ main.js
@@ -1,11 +1,7 @@
-const fs = require("fs");
-const { Client, MessageMedia } = require("whatsapp-web.js");
+const { Client } = require("whatsapp-web.js");
 const qrcode = require("qrcode-terminal");
 const axios = require("axios");
 require("dotenv").config();
-const { default: MonsterApiClient } = require("monsterapi");
-const clientImage = new MonsterApiClient(process.env.MONSTER_API);
-const model = "txt2img";
 
 const client = new Client();
 
@@ -33,46 +29,6 @@ client.on("message", async (msg) => {
       msg.reply(generatedText);
     } catch (error) {
       console.error("Error:", error.response.data.error.message);
-    }
-  }
-});
-
-client.on("message", async (msg) => {
-  if (msg.body.startsWith(".image")) {
-    const textImage = msg.body.substring(100);
-    msg.reply("⏳ Proses AI");
-    const input = {
-      prompt: textImage,
-      negprompt: "",
-      samples: 1,
-      steps: 50,
-      aspect_ratio: "square",
-      guidance_scale: 7.5,
-      seed: 2414,
-    };
-
-    try {
-      const response = await clientImage.generate(model, input);
-      const urlImage = response.output[0];
-
-      const imageResponse = await axios.get(urlImage, {
-        responseType: "arraybuffer",
-      });
-
-      const imageName = urlImage.split("/").pop();
-
-      fs.writeFileSync(`./image/ai/${imageName}`, imageResponse.data);
-
-      const media = MessageMedia.fromFilePath(`./image/ai/${imageName}`);
-
-      await client.sendMessage(msg.from, media);
-
-      fs.unlinkSync(`./image/ai/${imageName}`);
-    } catch (error) {
-      console.error("Error:", error);
-      msg.reply(
-        "Maaf, terjadi kesalahan dalam mengambil gambar atau mengirimnya."
-      );
     }
   }
 });
